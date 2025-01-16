26aa96a7585ad99e824331aa12faf5e8f2ba2892
UvisVei <uvis.veips@gmail.com>
2025-01-16 10:17:57 -0500

    Completed update api

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -147,3 +147,18 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.get_json()
         self.assertEqual(len(data), 5)
+
+    def test_update_account(self):
+        """It should Update an existing Account"""
+        # create an account
+        test_account = AccountFactory()
+        response = self.client.post(BASE_URL, json=test_account.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+        # update the account
+        new_account = response.get_json()
+        new_account["name"] = "Something Known"
+        response = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_account = response.get_json()
+        self.assertEqual(updated_account["name"], "Something Known")
